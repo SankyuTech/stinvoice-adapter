ad0e7344fba4a43087c5f8385abaaad87fde467e
warfee <warfee619916@gmail.com>
2024-10-29 12:57:06 +0800

    moving validator class to submission class

--- src/One/Submission.php
+++ src/One/Submission.php
@@ -353,4 +353,43 @@ class Submission extends Request
         return $this->responseWith($response);
     }
 
+    public function checkAccess(){
+
+
+        $endpoint = $this->client->endpoint();
+        $response = $this->client->httpClient()
+
+            ->request('POST', $endpoint.'/ping', [
+                'headers' => array_merge(
+                    $this->client->auth()->credentials(),
+                    [
+                        'Accept' => 'application/json',
+                        'Content-type' => 'application/x-www-form-urlencoded',
+                    ]
+                )
+            ]);
+
+        return $this->responseWith($response);
+
+    }
+
+    public function checkIdentificationParty($param){
+
+        $endpoint = $this->client->endpoint();
+        $response = $this->client->httpClient()
+            ->request('POST', $endpoint.'/validate-tax-no', [
+                'headers' => array_merge(
+                    $this->client->auth()->credentials(),
+                    [
+                        'Accept' => 'application/json',
+                        'Content-type' => 'application/x-www-form-urlencoded',
+                    ]
+                ),
+                'form_params' => $param
+            ]);
+
+        return $this->responseWith($response);
+
+    }
+
 }
